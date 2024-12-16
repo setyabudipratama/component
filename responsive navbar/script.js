604900432b485eebe41623358c18ba0df000102c
Setya Budi Pratama <setyabudipratam@gmail.com>
2024-12-16 14:06:10 +0700

    Delete responsive navbar/script.js

--- responsive navbar/script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById('toggle');
-const menu = document.querySelector('.menu ul');
-
-toggle.addEventListener('click', function() {
-  menu.classList.toggle('active');
-});
