62a22dfcf51227f3a5a142f682763f8557435578
Setya Budi Pratama <setyabudipratam@gmail.com>
2024-12-21 14:06:54 +0700

    Update script.js

--- social/script.js
+++ social/script.js
@@ -1,18 +1,7 @@
 const bars = document.querySelector('header .fa-bars-staggered');
 bars.addEventListener('click', function() {
     this.classList.toggle('fa-xmark');
-    const instagram = document.querySelector('header .fa-instagram');
-    instagram.classList.toggle('active');
-    const github = document.querySelector('header .fa-github');
-    github.classList.toggle('active');
-    const linkedin = document.querySelector('header .fa-linkedin');
-    linkedin.classList.toggle('active');
-    const telegram = document.querySelector('header .fa-telegram');
-    telegram.classList.toggle('active');
-    const facebook = document.querySelector('header .fa-facebook');
-    facebook.classList.toggle('active');
-    const twitter = document.querySelector('header .fa-x-twitter');
-    twitter.classList.toggle('active');
     const icons = document.querySelectorAll('header .fa-brands');
+    icons.forEach(icon => icon.classList.toggle('active'));
     bars.style.color = [...icons].some(icon => icon.classList.contains('active')) ? 'black' : 'white';
 });
