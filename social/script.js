0a8303581280509d5a76b166726153155aa59ce3
Setya Budi Pratama <setyabudipratam@gmail.com>
2024-12-29 14:01:16 +0700

    Delete social/script.js

--- social/script.js
+++
@@ -1,7 +0,0 @@
-const bars = document.querySelector('header .fa-bars-staggered');
-bars.addEventListener('click', function() {
-    this.classList.toggle('fa-xmark');
-    const icons = document.querySelectorAll('header .fa-brands');
-    icons.forEach(icon => icon.classList.toggle('active'));
-    bars.style.color = [...icons].some(icon => icon.classList.contains('active')) ? 'black' : 'white';
-});
