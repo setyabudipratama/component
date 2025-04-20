2992e97a279804c8ac19ad178c9f592a74f05d68
Setya Budi Pratama <setyabudipratam@gmail.com>
2025-04-20 18:03:10 +0700

    Update script.js

--- loginForm/05/script.js
+++ loginForm/05/script.js
@@ -4,3 +4,17 @@ document.querySelectorAll('.login form h5 span, .register form h5 span').forEach
         document.querySelector('.register').classList.toggle('active');
     })
 })
+document.querySelectorAll('.fa-eye').forEach(eye => {
+    eye.addEventListener('click', function () {
+        const passwordInputs = document.querySelectorAll
+        ('.login .pass input, .register .pass input, .register .pass2 input')
+        passwordInputs.forEach(passwordInput => {
+            passwordInputs.forEach(passwordInput => {
+                passwordInput.type = passwordInput.type === 'password' ? 'text' : 'password';
+                this.classList.toggle('fa-eye-slash', passwordInput.type === 'text');
+                this.classList.toggle('fa-eye', passwordInput.type !== 'text');
+                this.classList.toggle('active', passwordInput.type === 'text');
+            })
+        })
+    })
+})
