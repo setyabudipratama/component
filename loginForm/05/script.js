44d15b296c108528ee6d651e16a4cac5b14c56c2
Setya Budi Pratama <setyabudipratam@gmail.com>
2025-04-20 18:04:58 +0700

    Update script.js

--- loginForm/05/script.js
+++ loginForm/05/script.js
@@ -7,14 +7,12 @@ document.querySelectorAll('.login form h5 span, .register form h5 span').forEach
 document.querySelectorAll('.fa-eye').forEach(eye => {
     eye.addEventListener('click', function () {
         const passwordInputs = document.querySelectorAll
-        ('.login .pass input, .register .pass input, .register .pass2 input')
+            ('.login .pass input, .register .pass input, .register .pass2 input')
         passwordInputs.forEach(passwordInput => {
-            passwordInputs.forEach(passwordInput => {
-                passwordInput.type = passwordInput.type === 'password' ? 'text' : 'password';
-                this.classList.toggle('fa-eye-slash', passwordInput.type === 'text');
-                this.classList.toggle('fa-eye', passwordInput.type !== 'text');
-                this.classList.toggle('active', passwordInput.type === 'text');
-            })
+            passwordInput.type = passwordInput.type === 'password' ? 'text' : 'password';
+            this.classList.toggle('fa-eye-slash', passwordInput.type === 'text');
+            this.classList.toggle('fa-eye', passwordInput.type !== 'text');
+            this.classList.toggle('active', passwordInput.type === 'text');
         })
     })
 })
