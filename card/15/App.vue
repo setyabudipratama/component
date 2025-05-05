d1fb93886be545cb587872c306019107140eb9d3
Setya Budi Pratama <setyabudipratam@gmail.com>
2025-05-05 16:31:41 +0700

    Add files via upload

---
+++ card/15/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <card />
+</template>
+
+<script>
+import card from './components/card.vue'
+
+export default {
+  name: 'App',
+  components: {
+    card
+  }
+}
+</script>
---
+++ card/15/buttonCard.vue
@@ -0,0 +1,64 @@
+<template>
+    <button @click="handleClick('suka')" class="buttonCard fa-solid fa-heart">
+    </button>
+    <button @click="handleClick('tidak suka')" class="buttonCard fa-solid fa-thumbs-down">
+    </button>
+</template>
+
+<script>
+import Swal from 'sweetalert2'
+
+export default {
+    name: 'buttonCard',
+    methods: {
+        handleClick(type) {
+            if (type == 'suka') {
+                Swal.fire({
+                    title: 'Suka!',
+                    text: 'Saya menyukainya',
+                    color: 'black',
+                    background: 'yellow',
+                    confirmButtonText: 'Oke'
+                })
+            } else {
+                Swal.fire({
+                    title: 'Tidak Suka!',
+                    text: 'saya tidak suka',
+                    color: 'white',
+                    background: 'red',
+                    confirmButtonText: 'Oke'
+                })
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+button {
+    width: 120px;
+    height: 35px;
+    background-color: yellow;
+    border: none;
+    outline: none;
+    border-radius: 10px;
+    cursor: pointer;
+    box-shadow: 5px 5px 10px black;
+    transition: all 0.3s ease;
+}
+
+button:hover {
+    transform: scale(1.1);
+    box-shadow: none;
+}
+
+.fa-heart {
+    color: red;
+    font-size: 1.2rem;
+}
+
+.fa-thumbs-down {
+    color: darkblue;
+    font-size: 1.2rem;
+}
+</style>
