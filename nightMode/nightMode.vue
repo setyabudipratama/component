96d5320d4020371bf90e624d65504c9d4088ee57
Setya Budi Pratama <setyabudipratam@gmail.com>
2025-05-27 13:32:56 +0700

    Update nightMode.vue

--- nightMode/nightMode.vue
+++ nightMode/nightMode.vue
@@ -1,66 +1,66 @@
 <template>
-    <header class="lamp" :class="isNightMode ? 'dark' : 'light'">
-        <button @click="toggleActive">
-            <i class="fa-regular fa-lightbulb"></i>
-        </button>
-    </header>
+  <header class="lamp" :class="isNightMode ? 'dark' : 'light'">
+    <button @click="toggleActive">
+      <i class="fa-regular fa-lightbulb"></i>
+    </button>
+  </header>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 // tidak aktif pada awal
-const isNightMode = ref(false)
+const isNightMode = ref(false);
 
 // fungsi untuk mengubah darkmode
 function toggleActive() {
-    isNightMode.value = !isNightMode.value
-    
-    // ambil elemen app sebagai target
-    const app = document.querySelector('#app')
-    if (!app) return
-    app.classList.toggle('dark', isNightMode.value)
-    app.classList.toggle('light', !isNightMode.value)
+  isNightMode.value = !isNightMode.value;
+
+  // ambil elemen app sebagai target
+  const app = document.querySelector("#app");
+  if (!app) return;
+  app.classList.toggle("dark", isNightMode.value);
+  app.classList.toggle("light", !isNightMode.value);
 }
 </script>
 
-<style scope>
+<style>
 .lamp {
+  position: fixed;
+  width: 100%;
+  top: 0;
+  left: 0;
+
+  button {
     position: fixed;
-    width: 100%;
-    top: 0;
-    left: 0;
+    top: 7%;
+    left: 5%;
+    z-index: 1000;
+    width: 40px;
+    height: 40px;
+    background-color: yellow;
+    border: none;
+    outline: none;
+    border-radius: 50%;
+    cursor: pointer;
+    box-shadow: 2px 2px 10px black;
+    transition: all 0.3s ease;
 
-    button {
-        position: fixed;
-        top: 7%;
-        left: 5%;
-        z-index: 1000;
-        width: 40px;
-        height: 40px;
-        background-color: yellow;
-        border: none;
-        outline: none;
-        border-radius: 50%;
-        cursor: pointer;
-        box-shadow: 2px 2px 10px black;
-        transition: all 0.3s ease;
-
-        i {
-            font-size: 1.5rem;
-            color: black;
-        }
+    i {
+      font-size: 1.5rem;
+      color: black;
     }
+  }
 }
 
 /* panggil elemen app untuk mengubah background */
 #app.dark {
-    background-color: rgb(39, 39, 39);
-    color: white;
+  background-color: rgb(39, 39, 39);
+  color: white;
 }
 
 #app.light {
-    background-color: lightblue;
-    color: rgb(19, 45, 45);
+  background-color: lightblue;
+  color: rgb(19, 45, 45);
 }
 </style>
